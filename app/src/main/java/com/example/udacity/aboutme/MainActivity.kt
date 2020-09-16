808542f1bbb2d16325d32c8ede6ee31f1950289d
rukkachou <rukka.chou@gmail.com>
2020-09-17 01:25:59 +0800

    after ch.13 (@color/ View.visibility/ hide the keyboard)

--- app/src/main/java/com/example/udacity/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/udacity/aboutme/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.udacity.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            setNickName(it)
+        }
+    }
+    fun setNickName(view: View) {
+        val nicknameEdit: EditText = findViewById(R.id.nickname_edit)
+        val nicknameText: TextView = findViewById(R.id.nickname_text)
+
+        nicknameText.text = nicknameEdit.text
+        nicknameEdit.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameText.visibility = View.VISIBLE
+
+        // hide the keyboard
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
